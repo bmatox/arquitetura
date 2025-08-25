4620d66ebd4d98b1865691b3ab46452dcbf2ff69
51644 <51644@tjce.jus.br>
2025-08-25 20:26:23 -0300

    feat. configura a classe User.java, adiciona notações

--- src/main/java/org/example/arquitetura/model/User.java
+++ src/main/java/org/example/arquitetura/model/User.java
@@ -1,4 +1,17 @@
 package org.example.arquitetura.model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class User {
+
+    private Long id;
+    private String nome;
+    private String email;
+
+
 }
